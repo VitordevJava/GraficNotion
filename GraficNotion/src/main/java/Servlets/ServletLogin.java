ba88045e3e7994eb4040d45c3dd2461cb196243a
VitordevJava <vitordev.java@gmail.com>
2025-05-17 02:20:01 -0300

    Simulando login no sistema

--- GraficNotion/src/main/java/Servlets/ServletLogin.java
+++ GraficNotion/src/main/java/Servlets/ServletLogin.java
@@ -1,41 +1,68 @@
 package Servlets;
 
 import java.io.IOException;
+import java.net.ResponseCache;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ServletLogin
- */
+import model.ModelLogin;
+
+
 @WebServlet("/ServletLogin")
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+   
     public ServletLogin() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	// Recebe os dados pela Url em parâmetros
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	// Recebe os dados enviados por um formulario
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
+		
+	
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+
+		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+			
+			if(modelLogin.getLogin().equalsIgnoreCase("admin")
+					&& modelLogin.getSenha().equalsIgnoreCase("admin")) {
+
+				request.getSession().setAttribute("usuario", modelLogin.getLogin());
+				
+				RequestDispatcher redirecionar = request.getRequestDispatcher("principal/principal.jsp");
+				redirecionar.forward(request, response);
+				
+			}
+			else {
+				RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+				request.setAttribute("msg", "Os campos não podem ser vazios");
+				redirecionar.forward(request, response);
+
+			}
+		}
+		else {
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			request.setAttribute("msg", "Os campos não podem ser vazios");
+			redirecionar.forward(request, response);
+		}
 	}
 
 }
